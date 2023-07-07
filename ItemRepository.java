973feacc83f8405bbb4f5730ada63c8d8ce882e2
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-07-07 10:57:28 +0300

    Add files via upload

---
+++ ItemRepository.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ItemRepository {
+
+    protected List<Item<Product>> items;
+
+    public ItemRepository () {
+        items = new ArrayList<>();
+    }
+
+    //repository Logic
+
+
+    public void addItem(Item<Product> item){
+        items.add(item);  
+    }
+
+    // to get items
+    public void getItems(Integer index){
+        System.out.println(items.get(index));        
+    }
+
+
+    @Override
+    public String toString() {
+        return "\t\n\n [items =  " + items + "]\n";
+    }
+
+
+    public Item<Product> getItemById(Integer id){
+        Item<Product> item = null;
+
+        for (int i = 0; i < items.size(); i++) {
+           
+            if((items.get(i).getValue().getId()).equals(id)){
+                item = items.get(i);
+                System.out.println("The product with id "+id+" you searched for:");
+                break;
+            }else if(!(items.get(i).getValue().getId()).equals(id)){
+                System.err.println("You've entered wrong ID !!!");
+                break;
+            }
+            
+        }
+        return item;
+
+    }
+
+    //HW2: add removeItemById method;
+
+    public void removeItemById(Integer id){
+        Item<Product> item = null;
+        for (int index = 0; index < items.size(); index++) {
+            if((items.get(index).getValue().getId()).equals(id)){
+                item = items.get(index);
+                items.remove(item);
+                break;
+            } 
+        }
+        System.out.println("The product with ID : "+ id +" was REMOVED !!!");
+    }
+
+    public List<Item<Product>> getItems() {
+        return items;
+    }
+
+   
+    
+    
+
+    
+    
+}
