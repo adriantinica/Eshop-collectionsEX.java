dc04ff0c1696f18662add6b383b278f09afc0260
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-07-07 10:57:03 +0300

    Add files via upload

---
+++ Cart.java
@@ -0,0 +1,82 @@
+public class Cart extends ItemRepository {
+    
+    private Client owner;
+    private Money totalCost;
+
+    public Cart(Client owner) {
+        //super() - auto generated
+        this.owner = owner;
+        this.totalCost= new Money(000,Currency.EUR);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Cart [owner=" + owner +" \n\ttotalCost"+ totalCost + " ]" + super.toString() ;
+    }
+
+    // Cart Logic
+
+    public Client getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Client owner) {
+        this.owner = owner;
+    }
+
+    
+    @Override
+    public void addItem(Item<Product> item) {
+        // TODO Auto-generated method stub
+        
+        super.addItem(item);
+        totalCost.setAmount(totalCost.getAmount()+item.getValue().getPrice().getAmount()*item.getQuantity()
+        );
+    }
+
+    // HW3: draw the diagram of toString() delegation
+    //    : draw the diagram of constructor() delegation
+     //    : draw the diagram of addItem() delegation
+
+    //HW4: ADD removeItem(Item item)
+            //removes the indicated object
+            //update totalCost
+
+
+    
+    //@Override
+    //public void getItembyItem(Integer i) {
+    //    super.getItembyItem(i);
+    //    totalCost = totalCost.setAmount(.size());
+    //}
+    
+    
+
+
+    //public void removeItem(Item item){
+    //    item.getQuantity() - item;
+    //}
+
+
+    public void removeItem(Item<Product> item) {
+        for (int index = 0; index < items.size(); index++) {
+            if ((items.get(index)).equals(item)) {
+                Item<Product> removedItem = items.remove(index);
+                totalCost.setAmount(totalCost.getAmount() - (removedItem.getValue().getPrice().getAmount() * removedItem.getQuantity()));
+                break;
+            }
+        }
+        System.out.println("The item : "+ item +" was REMOVED !!!");
+    }
+
+    //HW5: ineaseItemQuantity(Item item, Integer amount);
+            // set protection 
+    //     decreaseItemQuantity(Item item, Integer amount);
+            // set protection
+
+
+    
+     
+    
+}
