866446dbde3b65ef13590499b6a6b3a26fef5add
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-07-07 11:33:02 +0300

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -2,6 +2,7 @@ public class Cart extends ItemRepository {
     
     private Client owner;
     private Money totalCost;
+    private Item<Product> itemToRemove;
 
     public Cart(Client owner) {
         //super() - auto generated
@@ -25,6 +26,11 @@ public class Cart extends ItemRepository {
         this.owner = owner;
     }
 
+    public void setItemToRemove(Item<Product> item) {
+        itemToRemove = item;
+    }
+
+
     
     @Override
     public void addItem(Item<Product> item) {
@@ -35,6 +41,26 @@ public class Cart extends ItemRepository {
         );
     }
 
+    public void removeItem() {
+        if (itemToRemove != null) {
+            super.removeItem(itemToRemove);
+            totalCost.setAmount(totalCost.getAmount() - (itemToRemove.getValue().getPrice().getAmount() * itemToRemove.getQuantity()));
+            itemToRemove = null; 
+        }
+    }
+
+    //public void removeItem(Item<Product> item) {
+   //     for (int index = 0; index < items.size(); index++) {
+    //        if ((items.get(index)).equals(item)) {
+    //            Item<Product> removedItem = items.remove(index);
+    //            totalCost.setAmount(totalCost.getAmount() - (removedItem.getValue().getPrice().getAmount() * removedItem.getQuantity()));
+    //            break;
+    //        }
+    //    }
+    //    System.out.println("The item : "+ item +" was REMOVED !!!");
+    //}
+
+
     // HW3: draw the diagram of toString() delegation
     //    : draw the diagram of constructor() delegation
      //    : draw the diagram of addItem() delegation
@@ -42,37 +68,11 @@ public class Cart extends ItemRepository {
     //HW4: ADD removeItem(Item item)
             //removes the indicated object
             //update totalCost
-
-
-    
-    //@Override
-    //public void getItembyItem(Integer i) {
-    //    super.getItembyItem(i);
-    //    totalCost = totalCost.setAmount(.size());
-    //}
-    
-    
-
-
-    //public void removeItem(Item item){
-    //    item.getQuantity() - item;
-    //}
-
-
-    public void removeItem(Item<Product> item) {
-        for (int index = 0; index < items.size(); index++) {
-            if ((items.get(index)).equals(item)) {
-                Item<Product> removedItem = items.remove(index);
-                totalCost.setAmount(totalCost.getAmount() - (removedItem.getValue().getPrice().getAmount() * removedItem.getQuantity()));
-                break;
-            }
-        }
-        System.out.println("The item : "+ item +" was REMOVED !!!");
-    }
+   
 
     //HW5: ineaseItemQuantity(Item item, Integer amount);
             // set protection 
-    //     decreaseItemQuantity(Item item, Integer amount);
+            // decreaseItemQuantity(Item item, Integer amount);
             // set protection
 
 
