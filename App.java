be439a8481a09dc2611c644615b8ff3de3a81705
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-07-09 20:49:04 +0300

    Update App.java
    Final changes

--- App.java
+++ App.java
@@ -8,14 +8,14 @@ public class App {
 
         Product product1  = new Product(1001,"Samsung XX11", new Money (1000, Currency.EUR));
         Product product2  = new Product (1002,"Apple 14 PRO", new Money (1600, Currency.EUR));
-        Product product3 = new Product(1003, "OPPO Reno 8 ", new Money(500, Currency.EUR));
+        //Product product3 = new Product(1003, "OPPO Reno 8 ", new Money(500, Currency.EUR));
 
         stock.addItem( 
             new Item<>(product1,100));
         stock.addItem(
-            new Item<>(product2,100));
-        stock.addItem(
-            new Item<>(product3,100));
+            new Item<>(product2,140));
+        //stock.addItem(
+            //new Item<>(product3,100));
 
 
         
@@ -26,31 +26,29 @@ public class App {
        
         System.out.println(stock);
 
-        Item<Product> item1 = new Item<>( product1,
-            120);
+        Item<Product> item1 = new Item<>( product1, 120);
         cart.addItem(item1);
             
-        Item<Product> item2 = new Item<>(product2,
-            80);
+        Item<Product> item2 = new Item<>(product2,80);
         cart.addItem(item2);
 
-         Item<Product> item3 = new Item<>(product3,
-            100);
-        cart.addItem(item3);
+        //Item<Product> item3 = new Item<>(product3,100);
+        //cart.addItem(item3);
         
         
         System.out.println("Real Stock after cart fill: ");
         System.out.println(stock);
         System.out.println(cart);
-        cart.removeItem(item3);
-        System.out.println("Situation after removing item3");
+        cart.decreaseItemQuantity(item2,79);
+        //cart.removeItem(item2);
+        //System.out.println("Situation after removing item3");
         System.out.println(stock);
         System.out.println(cart);
                 
 
         
        
-        
+    
 
        
 
